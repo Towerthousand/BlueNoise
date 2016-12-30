2d369c8eac8db4aac1741bfd551cec4611a495d6
Towerthousand <dani.torramilans@gmail.com>
2016-12-30 02:37:53 +0100

    Fullscreen

--- game/main.cpp
+++ game/main.cpp
@@ -6,11 +6,10 @@ int main() {
     ContextSettings cs;
     cs.versionMajor = 4;
     cs.versionMinor = 5;
-    Game* g = new Game(Window::DisplayMode::createWindowedMode(1000,1000), cs);
+    Game* g = new Game(Window::getFullscreenModes()[0], cs);
     Scene* s = new Scene();
     s->addTo(g);
     g->run();
     delete g;
     return 0;
 }
-
